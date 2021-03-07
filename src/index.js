f6c53eabfb2845d285151e23c84e447d31c8f74d
Darrel Idiagbor <idiagbordarrel@gmail.com>
2021-03-07 01:35:49 +0100

    initial commit and folder structure

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { hydrate, render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./stylesheet/main.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  );
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
